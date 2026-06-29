814f1d4e2ed3ae78580ee96cd40ebb6f25f0602f
watermelon <116296060+bytuananh712@users.noreply.github.com>
2026-06-29 13:38:48 +0700

    Đồng bộ cổng (Port) cho ứng dụng Backend và Frontend

---
+++ SBS.WebApp/Pages/Manager/Pools/Create.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SBS.WebApp.Pages.Manager.Pools
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
